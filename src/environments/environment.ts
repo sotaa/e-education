2e8505137c4a3b93800dc0d5d018450609907ef0
soheil najafi <soheilnajafimail2@gmail.com>
2020-04-26 10:37:57 +0430

    basic config for packages

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,38 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+const IDENTITY_SERVER_URL = "http://localhost:3000";
+
 export const environment = {
-  production: false
+  production: false,
+  identityUrls: {
+    login: IDENTITY_SERVER_URL.concat("/auth/login"),
+    register: IDENTITY_SERVER_URL.concat("/auth/register"),
+    // resetPassword: IDENTITY_SERVER_URL.concat('/auth/reset-password'),
+
+    createCourse: IDENTITY_SERVER_URL.concat("/courses"),
+    readCourses: IDENTITY_SERVER_URL.concat("/Courses"),
+    readCourse: IDENTITY_SERVER_URL.concat("/Courses/"),
+    deleteCourse: IDENTITY_SERVER_URL.concat("/Courses/"),
+
+    // createPaymentPlan: IDENTITY_SERVER_URL.concat('/payment-plans'),
+    // readPaymentPlans: IDENTITY_SERVER_URL.concat('/payment-plans'),
+    // readPaymentPlan: IDENTITY_SERVER_URL.concat('/payment-plans/'),
+    // deletePaymentPlan: IDENTITY_SERVER_URL.concat('/payment-plans/'),
+
+    // buyPlan: IDENTITY_SERVER_URL.concat('/payment/buy/'),
+
+    // createRole: IDENTITY_SERVER_URL.concat('/roles'),
+    // readRoles: IDENTITY_SERVER_URL.concat('/roles'),
+    // readRole: IDENTITY_SERVER_URL.concat('/roles/'),
+    // deleteRole: IDENTITY_SERVER_URL.concat('/roles/'),
+
+    // updateUserInfo: IDENTITY_SERVER_URL.concat('/users/'),
+    // readUsers: IDENTITY_SERVER_URL.concat('/users'),
+    // readUser: IDENTITY_SERVER_URL.concat('/users/'),
+    // deleteUser: IDENTITY_SERVER_URL.concat('/users/'),
+  },
+  authenticationPageUrl: "/auth",
 };
 
 /*
